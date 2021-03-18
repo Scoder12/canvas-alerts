1a9b13d4bddaeaff183721d131958cd9efb0528c
FrostyWinterz <67816160+FrostyWinterz@users.noreply.github.com>
2021-03-18 11:22:01 -0700

    Set up sessions and pasport

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import session from "express-session";
+
 const app = express();
+app.use(session({ secret: process.env.SESSION_SECRET || "" }));
 
 app.get("/", (_, res) => {
   res.send("Hello World");
