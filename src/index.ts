7e388f03d7bc44b034d683ed8bbd5a798b3f1280
FrostyWinterz <67816160+FrostyWinterz@users.noreply.github.com>
2021-03-19 08:39:55 -0700

    Deleted web portion and started on the discord bot

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,16 @@
-import express from "express";
-import session from "express-session";
-import passport from "passport";
-import oauth2 from "oauth2strategy";
+import { Client } from "discord.js";
 
-passport.use(
-  new OAuth2Strategy(
-    {
-      authorizationURL: "https://www.example.com/oauth2/authorize",
-      tokenURL: "https://www.example.com/oauth2/token",
-      clientID: EXAMPLE_CLIENT_ID,
-      clientSecret: EXAMPLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/example/callback",
-    },
-    function (accessToken, refreshToken, profile, cb) {
-      User.findOrCreate({ exampleId: profile.id }, function (err, user) {
-        return cb(err, user);
-      });
-    }
-  )
-);
+const client = new Client();
 
-const app = express();
-app.use(session({ secret: process.env.SESSION_SECRET || "" }));
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.get("/", (_, res) => {
-  res.send("Hello World");
+client.on("message", (msg) => {
+  if (msg.channel.type !== "dm") return;
+  if (msg.content === "Hello") {
+    msg.channel.send(`Hello, <@${msg.author.id}>`);
+  }
 });
 
-app.listen(3000);
+client.on("ready", () => {
+  console.log("Bot logged in");
+});
+
+client.login(process.env.TOKEN);
