01bed649790ffb426a4fb5751ef7e48b27fca961
FrostyWinterz <67816160+FrostyWinterz@users.noreply.github.com>
2021-05-24 15:24:45 -0700

    Created help and fixed assignments command

--- src/index.ts
+++ src/index.ts
@@ -18,21 +18,22 @@ interface Course {
 }
 type AssignmentWithName = Assignment & { className: string };
 
-async function getApi(route: string, param: string) {
+async function getApi(route: string, param: string, auth: string) {
   const r = await fetch(
-    `https://${process.env.CANVAS_URL}${route}?access_token=${process.env.CANVAS_KEY}&per_page=999${param}`
+    `https://${process.env.CANVAS_URL}${route}?access_token=${auth}&per_page=999${param}`
   );
   return await r.json();
 }
 
-async function getAllAssignments(): Promise<AssignmentWithName[]> {
-  const classes: Course[] = await getApi(`/api/v1/courses`, "");
+async function getAllAssignments(auth: string): Promise<AssignmentWithName[]> {
+  const classes: Course[] = await getApi(`/api/v1/courses`, "", auth);
   // console.log(classes);
   const assignments = await Promise.all(
     classes.map(async ({ id, name }) => {
       const api: Assignment[] = await getApi(
         `/api/v1/courses/${id}/assignments`,
-        `&bucket=future`
+        `&bucket=future`,
+        auth
       );
       return api
         .filter(({ due_at }) => {
@@ -91,12 +92,26 @@ client.on("message", async (msg) => {
     `);
   }
   if (msg.content.startsWith("assignments")) {
-    // let foo = msg.content.split(" ");
-    // let auth = foo[1];
+    let foo = msg.content.split(" ");
+    let auth = foo[1];
+    if (!auth) {
+      msg.channel.send("Error please provide your auth token.");
+      return;
+    }
     msg.channel.send("loading..........");
-    getAllAssignments().then((assignments: AssignmentWithName[]) => {
-      msg.channel.send(assignments.map(messageGen));
-    });
+    getAllAssignments(auth)
+      .then((assignments: AssignmentWithName[]) => {
+        msg.channel.send(assignments.map(messageGen));
+      })
+      .catch(() => msg.channel.send("Please provide a valid token"));
+  }
+  if (msg.content == "help") {
+    msg.channel.send(`Welcome to Spencer and Finn's canvas Assignment Bot
+Step 1 to using this bot is going to ${process.env.CANVAS_URL} and then clicking on your acount then settings
+from there you  need to scroll down till you see Approved Integrations and then press the blue "New Access Token" button.
+From there you will input a purpose(This can be whatever) then inpute the experation date(When it expires you will have to make a new one)
+then click generate token, BE SURE NOT TO CLOSE OUT OF THIS BEFORE YOU COPY YOUR TOKEN(make sure to save this somewhere, and dont let others access it.)
+Step 2 then type assignments followed by your token`);
   }
 });
 
